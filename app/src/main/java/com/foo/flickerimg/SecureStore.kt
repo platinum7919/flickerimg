ca464f4b3a72167fe87195d3e042189b312e93b2
Pan <ng.yat.pan@gmail.com>
2018-11-21 02:26:21 +0800

    Add a bit of comment

--- app/src/main/java/com/foo/flickerimg/SecureStore.kt
+++ app/src/main/java/com/foo/flickerimg/SecureStore.kt
@@ -1,5 +1,8 @@
 package com.foo.flickerimg
 
+/**
+ * Not secure yet, but it should. Use NDK perhaps?
+ */
 object SecureStore {
     fun getFlickrApiKey(): String {
         return "fee10de350d1f31d5fec0eaf330d2dba"
